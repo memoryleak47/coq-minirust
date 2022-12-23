4260a174f3cef0a6004d20c1ce5fe535c94b55e0
memoryleak47 <memoryleak47@protonmail.ch>
2022-12-23 15:41:32 +0100

    fix formatting

--- int_encoding.v
+++ int_encoding.v
@@ -22,12 +22,12 @@ Definition unsigned_stop (size: Size) : Int :=
   (2^(size*8))%Z.
 
 Definition signed_start (size: Size) : Int :=
-   let size := Z.of_nat size in
+  let size := Z.of_nat size in
   (-2^(size*8 - 1))%Z.
 
 (* stop is exclusive! Hence |start| = |stop| *)
 Definition signed_stop (size: Size) : Int :=
-   let size := Z.of_nat size in
+  let size := Z.of_nat size in
   ((2^(size*8 - 1)))%Z.
 
 (* the value by which a negative signed number is offsetted, to become positive. *)
@@ -52,25 +52,26 @@ Definition encode_uint_le (size : Size) (i : Int): list AbstractByte :=
 
 Definition encode_int_le (size: Size) (signedness: Signedness) (i : Int) : option (list AbstractByte) :=
   match signedness with
-   | Unsigned =>
-     let stop := unsigned_stop size in
-     match ((i >=? 0)%Z && (i <? stop)%Z) with
-       | true => Some (encode_uint_le size i)
-       | false => None
-     end
-
-   | Signed =>
-     let start := signed_start size in
-     let stop := signed_stop size in
-     let offset := signed_offset size in
-     match (i >=? 0)%Z with
-      | true => match (i <? stop)%Z with
-        | true => Some (encode_uint_le size i)
-        | false => None
+  | Unsigned =>
+    let stop := unsigned_stop size in
+    match ((i >=? 0)%Z && (i <? stop)%Z) with
+    | true => Some (encode_uint_le size i)
+    | false => None
+    end
+  | Signed =>
+    let start := signed_start size in
+    let stop := signed_stop size in
+    let offset := signed_offset size in
+    match (i >=? 0)%Z with
+    | true =>
+      match (i <? stop)%Z with
+      | true => Some (encode_uint_le size i)
+      | false => None
       end
-      | false => match (i >=? start)%Z with
-        | true => Some (encode_uint_le size (i + offset)%Z)
-        | false => None
+    | false =>
+      match (i >=? start)%Z with
+      | true => Some (encode_uint_le size (i + offset)%Z)
+      | false => None
       end
     end
   end.
@@ -78,15 +79,15 @@ Definition encode_int_le (size: Size) (signedness: Signedness) (i : Int) : optio
 Definition encode_int2 (size: Size) (signedness: Signedness) (i : Int) : option (list AbstractByte) :=
   let bytes := (encode_int_le size signedness i) in
   match ENDIANNESS with
-   | BigEndian => option_map (fun x => rev x) bytes (* this `fun` seems redundant, but it isn't *)
-   | LittleEndian => bytes
+  | BigEndian => option_map (fun x => rev x) bytes (* this `fun` seems redundant, but it isn't *)
+  | LittleEndian => bytes
   end.
 
 Definition encode_int (size: Size) (signedness: Signedness) (v: Value) : option (list AbstractByte) :=
- match v with
+  match v with
   | VInt x => encode_int2 size signedness x
   | _ => None
- end.
+  end.
 
 (* decode *)
 
@@ -99,13 +100,13 @@ Fixpoint unwrap (l: list AbstractByte) : option (list byte) :=
 
 Definition decode_uint_le (size: Size) (bytes: list AbstractByte): option Int :=
   match unwrap bytes with
-   | None => None
-   | Some bytes =>
-     let bytes := map Ascii.ascii_of_byte bytes in
-     let bytes := Vector.of_list bytes in
-     let n := ByteV2N bytes in
-     let n := Z.of_N n in
-     Some n
+  | None => None
+  | Some bytes =>
+    let bytes := map Ascii.ascii_of_byte bytes in
+    let bytes := Vector.of_list bytes in
+    let n := ByteV2N bytes in
+    let n := Z.of_N n in
+    Some n
   end.
 
 Definition decode_int_le (size: Size) (signedness: Signedness) (l: list AbstractByte) : option Int :=
@@ -126,11 +127,11 @@ Definition decode_int_le (size: Size) (signedness: Signedness) (l: list Abstract
   end.
 
 Definition decode_int (size: Size) (signedness: Signedness) (l: list AbstractByte) : option Value :=
-  let l := match ENDIANNESS with
-   | BigEndian => rev l
-   | LittleEndian => l
-  end in
+  let l :=
+    match ENDIANNESS with
+    | BigEndian => rev l
+    | LittleEndian => l
+    end in
 
   let opt_i := decode_int_le size signedness l in
   option_map VInt opt_i.
-
