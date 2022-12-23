c3793a1909b5d819fd6490bedfa3868298bdfdb2
memoryleak47 <memoryleak47@protonmail.ch>
2022-12-23 07:23:54 +0100

    small int_encoding steps

--- int_encoding.v
+++ int_encoding.v
@@ -16,22 +16,26 @@ Inductive IntervalResult (x start stop : Int): Type :=
  | IResIn : (x >= start)%Z -> (x < stop)%Z -> IntervalResult _ _ _
  | IResHigher : (x >= stop)%Z -> IntervalResult _ _ _.
 
-Definition mk_interval_result (x start stop: Int) : (* (start < stop)%Z -> *) IntervalResult x start stop.
+Definition mk_interval_result (x start stop: Int) : (start < stop)%Z -> IntervalResult x start stop.
 Admitted.
 
 Definition unsigned_stop (size: Size) : Int :=
   let size := Z.of_nat size in
   (2^(size*8))%Z.
 
+Lemma unsigned_stop_pos (size: Size) : (0 < (unsigned_stop size))%Z.
+Proof. unfold unsigned_stop. lia. Qed.
+
 Definition encode_uint_be (size : Size) (i : Int) (p1: (i >= 0)%Z) (p2 : (i < (unsigned_stop size))%Z): option (list AbstractByte). Admitted.
 
 Definition encode_int_be (int_ty : IntTy) (i : Int) : option (list AbstractByte) :=
+  let size := i_size int_ty in
   match i_signedness int_ty with
    | Unsigned =>
-     let stop := unsigned_stop (i_size int_ty) in
-     match mk_interval_result i 0%Z stop with
+     let stop := unsigned_stop size in
+     match mk_interval_result i 0%Z stop (unsigned_stop_pos size) with
        | IResLower _ _ _ _  => None
-       | IResIn _ _ _ p1 p2 => encode_uint_be (i_size int_ty) i p1 p2 
+       | IResIn _ _ _ p1 p2 => encode_uint_be size i p1 p2
        | IResHigher _ _ _ _ => None
      end
    | Signed => None (* TODO *)
