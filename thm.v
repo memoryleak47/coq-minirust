60ac40f61cb8c3e27b7b448b537c10f214e8ba81
memoryleak47 <memoryleak47@protonmail.ch>
2022-12-27 01:24:33 +0100

    update thm.v

--- thm.v
+++ thm.v
@@ -1,74 +1,31 @@
-(*
 Require Import defs.
+Require Import encoding.
 Require Import le.
-Require Import lemma.
 
-Require Import Coq.Bool.Bool.
-Require Import Coq.Init.Datatypes.
-Require Import Coq.Init.Byte.
-Require Import Coq.Lists.List.
-Import ListNotations.
+Definition mono1 (ty: Ty) :=
+  forall (v1 v2: Value),
+  (le v1 v2)
+  -> (is_valid_for ty v1)
+  -> (is_valid_for ty v2)
+  -> exists (v1_l v2_l: list AbstractByte),
+  encode ty v1 = Some v1_l
+  /\ encode ty v2 = Some v2_l
+  /\ le v1_l v2_l.
 
-Theorem mono1 (ty: Ty) (v1 v2: Value) (p: le_val v1 v2) (p1: is_valid_for ty v1) (p2: is_valid_for ty v2) :
-exists e1, exists e2, encode ty v1 = Some e1 /\ encode ty v2 = Some e2 /\ le_list e1 e2.
-Proof.
-(*
-destruct v1,v2.
-unfold le_val in p.
-simpl in p.
-replace b0 with b.
-apply obvious.
-Qed.
-*)
-Abort.
+Definition mono2 (ty: Ty) :=
+  forall (l1 l2: list AbstractByte),
+  (le l1 l2)
+  -> le (decode ty l1) (decode ty l2).
 
-Theorem mono2 (ty: Ty) (l1 l2: list AbstractByte) (p: le_list l1 l2) : le_opt (decode ty l1) (decode ty l2).
-Proof.
-(*
-destruct ty.
-destruct (mk_le_list l1 l2 p) as [|x1 x2 l1' l2' p' H].
-- unfold le_opt,decode. assumption.
-- destruct p'; clear p.
-  destruct H.
--- unfold le_opt,decode. auto.
--- unfold decode. destruct x; unfold le_opt; try auto.
---- unfold le_val. reflexivity.
---- unfold le_val. reflexivity.
--- unfold decode. destruct x; unfold le_opt; try auto.
---- unfold le_val. reflexivity.
---- unfold le_val. reflexivity.
--- unfold decode.
-destruct x1; destruct x2; try destruct b; unfold le_opt; auto.
-Qed.
-*)
-Abort.
+Definition rt1 (ty: Ty) :=
+  forall (v: Value),
+  (is_valid_for ty v)
+  -> exists (v_l: list AbstractByte),
+  encode ty v = Some v_l
+  /\ decode ty v_l = Some v.
 
-Theorem roundtrip1 (ty: Ty) (v: Value) (p: is_valid_for ty v) :
-exists e, encode ty v = Some e /\ decode ty e = Some v.
-Proof.
-(*
-destruct ty.
-destruct v.
-destruct b; unfold encode,decode; reflexivity.
-Qed.
-*)
-Abort.
-
-Theorem roundtrip2 (ty: Ty) (l: list AbstractByte) (v: Value) (H: decode ty l = Some v) :
-exists e, encode ty v = Some e /\ le_list e l.
-Proof.
-(*
-destruct ty.
-destruct v.
-destruct (mk_decode_bool l).
-- unfold decode in H. inversion H.
-  unfold encode. unfold le_list. split; try auto.
-  unfold le_abstract_byte. reflexivity.
-- unfold decode in H. inversion H.
-  unfold encode. unfold le_list. split; try auto.
-  unfold le_abstract_byte. reflexivity.
-- rewrite e in H. discriminate H.
-Qed.
-*)
-Abort.
-*)
+Definition rt2 (ty: Ty) :=
+  forall (l: list AbstractByte) (v: Value),
+  (decode ty l = Some v)
+  -> exists (v_l: list AbstractByte),
+  encode ty v = Some v_l /\ le v_l l.
