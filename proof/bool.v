ab3f95fe4e52bd679786c8a8885a152c800b434d
memoryleak47 <memoryleak47@protonmail.ch>
2022-12-27 08:36:38 +0100

    proof thms for bool

---
+++ proof/bool.v
@@ -0,0 +1,99 @@
+Require Import defs encoding thm lemma.
+Require Import Coq.Init.Byte.
+Require Import List.
+Import ListNotations.
+
+Inductive BoolCases1 : list AbstractByte -> Type :=
+  | BC1True (p: option P) : BoolCases1 [Init x01 p]
+  | BC1False (p: option P) : BoolCases1 [Init x00 p]
+  | BC1Invalid (l: list AbstractByte) (H: decode TBool l = None) : BoolCases1 l.
+
+Lemma mk_bool_cases1 (l: list AbstractByte) : BoolCases1 l.
+Proof.
+destruct l.
+- apply BC1Invalid. auto.
+- destruct l.
+-- destruct a.
+--- apply BC1Invalid. auto.
+--- destruct b; try (apply BC1Invalid; reflexivity).
+---- apply BC1False.
+---- apply BC1True.
+-- apply BC1Invalid. simpl.
+   destruct a.
+--- reflexivity.
+--- destruct b; reflexivity.
+Qed.
+
+(* Lemma destruct_bool (v: Value) : is_valid_for TBool v -> exists b, v = VBool b. *)
+Lemma valid_bool (v: Value) : is_valid_for TBool v -> exists b, v = VBool b.
+Proof.
+unfold is_valid_for.
+unfold decode.
+unfold decode_bool.
+intros Hl.
+destruct Hl as [l].
+destruct (mk_bool_cases1 l).
+- inversion H. exists true. reflexivity.
+- inversion H. exists false. reflexivity.
+- unfold decode in H0.
+  unfold decode_bool in H0.
+  rewrite H in H0. discriminate H0.
+Qed.
+
+Lemma bool_mono1 : mono1 TBool.
+Proof.
+unfold mono1.
+intros v1 v2 HLe valid1 valid2.
+destruct (valid_bool v1 valid1) as [b1 H1].
+destruct (valid_bool v2 valid2) as [b2 H2].
+rewrite H1,H2 in HLe. simpl in HLe.
+rewrite <- HLe in H2.
+rewrite H1,H2.
+destruct b1.
+- do 2 exists [Init x01 None]. simpl. split; auto.
+- do 2 exists [Init x00 None]. simpl. split; auto.
+Qed.
+
+Lemma bool_mono2 : mono2 TBool.
+Proof.
+unfold mono2.
+intros l1 l2 HLe.
+destruct (mk_le_list _ _ HLe).
+- auto.
+- destruct l.
+-- destruct l0.
+--- simpl. auto.
+--- destruct x; simpl; auto.
+--- destruct x; simpl; auto.
+-- unfold decode.
+   unfold decode_bool.
+   destruct b1;
+   destruct b2;
+   simpl;
+   auto;
+   destruct b;
+   auto.
+Qed.
+
+Lemma bool_rt1 : rt1 TBool.
+Proof.
+unfold rt1.
+intros v valid.
+destruct (valid_bool v valid) as [b H].
+rewrite H. clear H valid v.
+destruct b.
+- exists [Init x01 None]. auto.
+- exists [Init x00 None]. auto.
+Qed.
+
+Lemma bool_rt2 : rt2 TBool.
+Proof.
+unfold rt2.
+intros l v H.
+destruct (mk_bool_cases1 l).
+- inversion H. clear v H H1.
+  exists [Init x01 None]. simpl. split; reflexivity || auto.
+- inversion H. clear v H H1.
+  exists [Init x00 None]. simpl. split; reflexivity || auto.
+- rewrite H in H0. discriminate H0.
+Qed.
--- proof/lemma.v
+++ proof/lemma.v
@@ -6,55 +6,18 @@ Require Import Coq.Init.Byte.
 Require Import Coq.Lists.List.
 Import ListNotations.
 
-(*
-
-(* TODO fixup lemmas! *)
-(* <= of vals *)
-
-Inductive LeVal : Value -> Value -> Type :=
-  | LValEq v : LeVal v v.
-
-Lemma mk_le_val (v1 v2 : Value) (p: le_val v1 v2) : LeVal v1 v2.
-Proof.
-(*
-destruct v1 as [b1|b1]; destruct v2 as [b2|b2];
-unfold le_val in p.
-- rewrite p. apply LValEq.
-- destruct p.
-- destruct p.
-- rewrite p. apply LValEq.
-Qed.
-*)
-Admitted.
-
-Inductive LeOpt : option Value -> option Value -> Type :=
-  | LNone o : LeOpt None o
-  | LSome i1 i2 : (LeVal i1 i2) -> LeOpt (Some i1) (Some i2).
-
-Lemma mk_le_opt (o1 o2 : option Value) (p: le_opt o1 o2) : LeOpt o1 o2.
-Proof.
-destruct o1.
-- destruct o2.
--- unfold le_opt in p. destruct (mk_le_val _ _ p).
---- apply LSome. apply mk_le_val. assumption.
--- unfold le_opt in p. destruct p.
-- apply LNone.
-Qed.
-
-(* <= of byte lists *)
-
 Inductive LeAbstractByte : AbstractByte -> AbstractByte -> Type :=
   | LUninit b : LeAbstractByte Uninit b
   | LNoProv x o : LeAbstractByte (Init x None) (Init x o)
   | LWithProv x p : LeAbstractByte (Init x (Some p)) (Init x (Some p)).
 
-Lemma mk_le_abstract_byte (b1 b2 : AbstractByte) (p: le_abstract_byte b1 b2) : LeAbstractByte b1 b2.
+Lemma mk_le_abstract_byte (b1 b2 : AbstractByte) (p: le b1 b2) : LeAbstractByte b1 b2.
 Proof.
 destruct b1.
 - apply LUninit.
 - destruct b2.
--- unfold le_abstract_byte in p. destruct o; destruct p.
--- unfold le_abstract_byte in p.
+-- unfold le in p. destruct o; destruct p.
+-- unfold le in p.
 destruct o.
 --- destruct o0.
 ---- destruct p. rewrite H. rewrite H0. apply LWithProv.
@@ -66,7 +29,7 @@ Inductive LeList : list AbstractByte -> list AbstractByte -> Type :=
   | LEmpty : LeList [] []
   | LLe b1 b2 l1' l2' : LeList l1' l2' -> LeAbstractByte b1 b2 -> LeList (b1::l1') (b2::l2').
 
-Lemma mk_le_list (l1 : list AbstractByte) : forall l2, le_list l1 l2 -> LeList l1 l2.
+Lemma mk_le_list (l1 : list AbstractByte) : forall l2, le l1 l2 -> LeList l1 l2.
 Proof.
 induction l1 as [|b1 l1' H].
 - intros l2 p. destruct l2 as [|b2 l2'].
@@ -78,54 +41,3 @@ induction l1 as [|b1 l1' H].
 --- apply H. destruct p. assumption.
 --- destruct p. apply mk_le_abstract_byte. assumption.
 Qed.
-
-(* decoding *)
-
-Inductive DecodeBool : list AbstractByte -> Type :=
- | DBoolTrue o : DecodeBool [Init x01 o]
- | DBoolFalse o : DecodeBool [Init x00 o]
- | DBoolInvalid l : (decode BoolTy l = None) -> DecodeBool l.
-
-Lemma mk_decode_bool (l: list AbstractByte) : DecodeBool l.
-Proof.
-destruct l.
-- apply DBoolInvalid. auto.
-- destruct l.
--- destruct a.
---- apply DBoolInvalid. auto.
---- destruct b; try (apply DBoolInvalid; unfold decode; reflexivity).
----- apply DBoolFalse.
----- apply DBoolTrue.
--- apply DBoolInvalid.
-unfold decode. destruct a.
---- reflexivity.
---- destruct b; reflexivity.
-Qed.
-
-(* deprecated: *)
-
-Lemma obvious (l: list AbstractByte) : le_list l l.
-Proof.
-induction l.
-- unfold le_list. auto.
-- unfold le_list. split.
--- destruct a.
---- unfold le_abstract_byte. auto.
---- unfold le_abstract_byte.
-    destruct o. auto. auto.
--- auto.
-Qed.
-
-Lemma le_list_helper (l1 : list AbstractByte) : forall l2, le_list l1 l2 -> length l1 = length l2.
-Proof.
-induction l1 as [|b1 l1' H].
-- intros l2 p. destruct l2 as [|b2 l2'].
--- reflexivity.
--- destruct p.
-- intros l2 p. destruct l2 as [|b2 l2'].
--- destruct p.
--- simpl. f_equal.
-apply H. destruct p as [A B].
-assumption.
-Qed.
-*)
