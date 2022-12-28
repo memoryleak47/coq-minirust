eccafd9d5f21f512940703231beb588b8aa41c7f
memoryleak47 <memoryleak47@protonmail.ch>
2022-12-28 15:32:55 +0100

    work on rt1_uint_le

--- proof/int_raw.v
+++ proof/int_raw.v
@@ -3,6 +3,8 @@ Require Import Coq.Logic.FunctionalExtensionality.
 Require Import Coq.Logic.EqdepFacts.
 Require Import List.
 Require Import Ndigits.
+Require Import ZArith.
+Require Import Lia.
 
 Lemma lemma1 (l: list Ascii.ascii): (map Ascii.ascii_of_byte (map Ascii.byte_of_ascii l)) = l.
 Proof.
@@ -36,10 +38,34 @@ rewrite Vector.of_list_to_list_opp.
 reflexivity.
 Qed.
 
+Lemma lemma5 (n:nat) (v: Vector.t bool (n*8)) :
+  ByteVector.to_Bvector (ByteVector.of_Bvector v) = v.
+Proof.
+Admitted.
+
+Lemma lemma6 (size: Size) (v: BinNums.N) : Bv2N (N2Bv_sized (size * 8) v) = v.
+Proof.
+Admitted.
+
+Lemma destruct_int_in_range (i: Int) (size: Size) (signed: Signedness) (P: int_in_range i size signed = true) :
+(i >= int_start size signed)%Z /\ (i < int_stop size signed)%Z.
+Proof.
+unfold int_in_range in P.
+lia.
+Qed.
+
 Lemma rt1_uint_le (size: Size) (int: Int): (int_in_range int size Unsigned = true) -> decode_uint_le size (encode_uint_le size int) = int.
 Proof.
 intros H.
 unfold decode_uint_le, encode_uint_le.
 rewrite lemma1.
 rewrite lemma4.
-Abort.
+unfold ByteV2N.
+unfold N2ByteV_sized.
+unfold Basics.compose.
+rewrite lemma5.
+rewrite lemma6.
+rewrite Znat.Z2N.id. { reflexivity. }
+destruct (destruct_int_in_range _ _ _ H).
+unfold int_start in H0. lia.
+Qed.
