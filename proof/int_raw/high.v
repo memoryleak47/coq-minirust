78a3c3502b8aaad5f6d276c9dad9d85a0c0ef4c7
memoryleak47 <memoryleak47@protonmail.ch>
2022-12-31 21:19:40 +0100

    int_raw/high.v rt properties now contain the missing props too

--- proof/int_raw/high.v
+++ proof/int_raw/high.v
@@ -7,14 +7,53 @@ Require Import Ndigits.
 Require Import ZArith.
 Require Import Lia.
 
-(* encode yes, no *)
+(* roundtrip properties *)
 
-Lemma encode_int_yes {size: Size} {signedness: Signedness} {int: Int} (H: int_in_range int size signedness = true) :
-  exists l, Some l = encode_int_raw size signedness int /\ length l = size.
+Lemma rt1_int (size: Size) (signedness: Signedness) (int: Int) (H: int_in_range int size signedness = true) :
+  (size > 0) ->
+  exists l, Some l = encode_int_raw size signedness int /\
+  decode_int_raw size signedness l = Some int /\
+  length l = size.
 Proof.
-Admitted.
+intros Hs.
+destruct ENDIANNESS eqn:E.
+- destruct (rt1_int_le size signedness int H) as [l [H0 [H1 H2]]]; try assumption.
+  exists l. unfold encode_int_raw,decode_int_raw. rewrite E. split; try split; assumption.
+- destruct (rt1_int_le size signedness int H) as [l [H0 [H1 H2]]]; try assumption.
+  exists (rev l). unfold encode_int_raw,decode_int_raw. rewrite E.
+  rewrite <- H0. simpl.
+  split; try reflexivity.
+  rewrite rev_involutive.
+  split. { assumption. }
+  rewrite rev_length.
+  assumption.
+Qed.
 
-Lemma encode_int_no {size: Size} {signedness: Signedness} {int: Int} (H: int_in_range int size signedness = false) :
+Lemma rt2_int (size: Size) (signedness: Signedness) (l: list byte) (H: length l = size) :
+  (size > 0) ->
+  exists i, Some i = decode_int_raw size signedness l /\
+  encode_int_raw size signedness i = Some l /\
+  int_in_range i size signedness = true.
+Proof.
+intros Hs.
+destruct ENDIANNESS eqn:E.
+- destruct (rt2_int_le size signedness l H) as [i [H0 [H1 H2]]]; try assumption.
+  exists i. unfold encode_int_raw,decode_int_raw. rewrite E. split; try split; assumption.
+- assert (length (rev l) = size) as H'. { rewrite rev_length. assumption. }
+  destruct (rt2_int_le size signedness (rev l) H') as [i [H0 [H1 H2]]]; try assumption.
+  exists i. unfold encode_int_raw,decode_int_raw. rewrite E.
+  rewrite <- H0.
+  split. { reflexivity. }
+  rewrite H1.
+  simpl.
+  rewrite rev_involutive.
+  split. { reflexivity. }
+  assumption.
+Qed.
+
+(* none lemmata *)
+
+Lemma encode_int_none {size: Size} {signedness: Signedness} {int: Int} (H: int_in_range int size signedness = false) :
   encode_int_raw size signedness int = None.
 Proof.
 unfold encode_int_raw.
@@ -25,13 +64,7 @@ simpl;
 reflexivity.
 Qed.
 
-(* decode yes, no *)
-Lemma decode_int_yes {size: Size} {signedness: Signedness} {l: list byte} (H: length l = size) :
-  exists i, Some i = decode_int_raw size signedness l /\ int_in_range i size signedness = true.
-Proof.
-Admitted.
-
-Lemma decode_int_no {size: Size} {signedness: Signedness} {l: list byte} (H: length l <> size) :
+Lemma decode_int_none {size: Size} {signedness: Signedness} {l: list byte} (H: length l <> size) :
   decode_int_raw size signedness l = None.
 Proof.
 unfold decode_int_raw.
@@ -49,43 +82,4 @@ unfold decode_int_le;
 try (rewrite R || rewrite R');
 simpl;
 reflexivity.
-Qed.
-
-(* roundtrip properties *)
-
-Lemma rt1_int (size: Size) (signedness: Signedness) (int: Int) (H: int_in_range int size signedness = true) :
-  (size > 0) ->
-  exists l, Some l = encode_int_raw size signedness int /\
-  decode_int_raw size signedness l = Some int.
-Proof.
-intros Hs.
-destruct ENDIANNESS eqn:E.
-- destruct (rt1_int_le size signedness int H) as [l [H0 [H1 H2]]]; try assumption.
-  exists l. unfold encode_int_raw,decode_int_raw. rewrite E. split; assumption.
-- destruct (rt1_int_le size signedness int H) as [l [H0 [H1 H2]]]; try assumption.
-  exists (rev l). unfold encode_int_raw,decode_int_raw. rewrite E.
-  rewrite <- H0. simpl.
-  split; try reflexivity.
-  rewrite rev_involutive.
-  assumption.
-Qed.
-
-Lemma rt2_int (size: Size) (signedness: Signedness) (l: list byte) (H: length l = size) :
-  (size > 0) ->
-  exists i, Some i = decode_int_raw size signedness l /\
-  encode_int_raw size signedness i = Some l.
-Proof.
-intros Hs.
-destruct ENDIANNESS eqn:E.
-- destruct (rt2_int_le size signedness l H) as [i [H0 [H1 H2]]]; try assumption.
-  exists i. unfold encode_int_raw,decode_int_raw. rewrite E. split; assumption.
-- assert (length (rev l) = size) as H'. { rewrite rev_length. assumption. }
-  destruct (rt2_int_le size signedness (rev l) H') as [i [H0 [H1 H2]]]; try assumption.
-  exists i. unfold encode_int_raw,decode_int_raw. rewrite E.
-  rewrite <- H0.
-  split. { reflexivity. }
-  rewrite H1.
-  simpl.
-  rewrite rev_involutive.
-  reflexivity.
 Qed.
