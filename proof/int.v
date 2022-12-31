afc0e882548e90ab6bd35fbbf21aeb751ea88df2
memoryleak47 <memoryleak47@protonmail.ch>
2023-01-01 00:13:06 +0100

    finish IntPair lemmata

--- proof/int.v
+++ proof/int.v
@@ -9,15 +9,38 @@ Lemma wf_int {size: Size} {signedness: Signedness} (Hwf: wf (TInt size signednes
 Admitted.
 
 Inductive IntPair : Size -> Signedness -> Value -> list AbstractByte -> Prop :=
- | mkIntPair {size: Size} {signedness: Signedness} {i: Int} {l: list AbstractByte} :
-  length l = size -> int_in_range i size signedness = true ->
-  (decode (TInt size signedness) l = Some (VInt i)) -> (encode (TInt size signedness) (VInt i) = Some l) -> IntPair size signedness (VInt i) l.
+ | mkIntPair {size: Size} {signedness: Signedness} {i: Int} {l: list AbstractByte} {bl: list byte} :
+  unwrap_abstract l = Some bl -> length l = size -> length bl = size -> int_in_range i size signedness = true ->
+  (decode (TInt size signedness) l = Some (VInt i)) -> ((encode (TInt size signedness) (VInt i)) = Some (wrap_abstract bl None)) -> IntPair size signedness (VInt i) l.
 
-Lemma lemma1 bl : length (wrap_abstract bl None) = length bl.
+Lemma wrap_len bl : length (wrap_abstract bl None) = length bl.
 Proof.
 induction bl as [|b bl IH].
 - reflexivity.
 - simpl. f_equal. rewrite IH. reflexivity.
+Qed.
+
+(* TODO simplify *)
+Lemma unwrap_len l bl : (unwrap_abstract l = Some bl) -> length bl = length l.
+Proof.
+generalize dependent bl.
+induction l as [|a l IH].
+- intros bl A.
+  simpl in A.
+  inversion A.
+  reflexivity.
+- intros bl A.
+  destruct a.
+-- simpl in A. discriminate A.
+-- simpl in A.
+   destruct bl.
+--- destruct (unwrap_abstract l).
+---- simpl in A. discriminate A.
+---- simpl in A. discriminate A.
+--- simpl. f_equal. simpl in A. rewrite IH. { reflexivity. }
+    destruct (unwrap_abstract l).
+---- simpl in A. inversion A. reflexivity.
+---- simpl in A. discriminate A.
 Qed.
 
 Lemma unwrap_wrap l : forall p, unwrap_abstract (wrap_abstract l p) = Some l.
@@ -29,7 +52,7 @@ induction l as [|b l IH].
 Qed.
 
 Lemma encode_int_pair {size: Size} {signedness: Signedness} {v: Value} {l: list AbstractByte} (H: encode (TInt size signedness) v = Some l) :
-  (size > 0) -> 
+  (size > 0) ->
   IntPair size signedness v l.
 Proof.
 intros Hs.
@@ -47,10 +70,9 @@ assert (wrap_abstract bl None = l) as Hlbl. {
   inversion H.
   reflexivity.
 }
-rewrite <- Hlbl.
-rewrite <- Hlbl in H, Henc. clear l Hlbl.
+rewrite <- Hlbl in H, Henc.
 assert (length (wrap_abstract bl None) = size). {
-  rewrite lemma1. assumption.
+  rewrite wrap_len. assumption.
 }
 
 destruct (mk_var (decode (TInt size signedness) (wrap_abstract bl None))) as [d Hd].
@@ -59,7 +81,55 @@ unfold decode,decode_int in Hd.
 rewrite unwrap_wrap in Hd. simpl in Hd.
 rewrite H2 in Hd. simpl in Hd.
 rewrite <- Hd in Hd'.
-apply mkIntPair; assumption.
+
+assert (unwrap_abstract l = Some bl) as F. {
+  rewrite <- Hlbl.
+  rewrite unwrap_wrap. reflexivity.
+}
+rewrite Hlbl in H0,Hd'.
+apply (@mkIntPair size signedness i l bl F H0 H3 HR Hd' Henc).
+Qed.
+
+Lemma decode_int_pair {size: Size} {signedness: Signedness} {v: Value} {l: list AbstractByte} (Hdec: decode (TInt size signedness) l = Some v) :
+  (size > 0) ->
+  IntPair size signedness v l.
+Proof.
+intros Hs.
+set (t := TInt size signedness).
+assert (decode t l = Some v) as Hdec'. { assumption. }
+unfold decode,decode_int in Hdec.
+destruct (unwrap_abstract l) as [bl|] eqn:Hbl; cycle 1. {
+  simpl in Hdec. discriminate Hdec.
+}
+simpl in Hdec.
+destruct (decode_int_raw size signedness bl); cycle 1. {
+  simpl in Hdec. discriminate Hdec.
+}
+simpl in Hdec.
+inversion Hdec.
+rewrite <- H0 in Hdec'. clear v Hdec H0.
+destruct (mk_var (unwrap_len l bl Hbl)) as [Hlen _].
+
+assert (length bl = size). {
+  unfold decode,decode_int in Hdec'. simpl in Hdec'.
+  rewrite Hbl in Hdec'. simpl in Hdec'.
+  destruct (PeanoNat.Nat.eq_dec (length bl) size).
+-- assumption.
+-- rewrite decode_int_none in Hdec'. simpl in Hdec'. discriminate Hdec'. assumption.
+}
+assert (length l = size). { rewrite <- H. rewrite Hlen. reflexivity. }
+destruct (rt2_int size signedness bl H Hs) as [i' [H1 [H2 H3]]].
+assert (i = i'). {
+unfold decode,decode_int in Hdec'. simpl in Hdec'.
+  rewrite Hbl in Hdec'. simpl in Hdec'. rewrite <- H1 in Hdec'. simpl in Hdec'.
+  inversion Hdec'.
+  reflexivity.
+}
+rewrite <- H4 in H1, H3, H2. clear i' H4.
+apply (mkIntPair Hbl H0 H H3 Hdec'); try assumption.
+unfold encode,encode_int.
+simpl.
+rewrite H2. simpl. f_equal.
 Qed.
 
 Lemma valid_int {size: Size} {signedness: Signedness} {v: Value} (H: is_valid_for (TInt size signedness) v) :
