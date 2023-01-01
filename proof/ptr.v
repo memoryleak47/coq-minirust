3e442895bb72c5945476adadc6034963ba45a783
memoryleak47 <memoryleak47@protonmail.ch>
2023-01-01 05:12:11 +0100

    start on ValidPtr

--- proof/ptr.v
+++ proof/ptr.v
@@ -4,6 +4,17 @@ Require Import List.
 Require Import ZArith.
 Import ListNotations.
 
+Inductive Constraints : PtrTy -> Int -> Type :=
+  | CRaw {align} {size} {addr} : Constraints (Raw align size) addr.
+ (* TODO add other Constraints option *)
+
+Inductive ValidPtr : PtrTy -> Value -> Type :=
+  | mkValidPtr ptr_ty addr p : Constraints ptr_ty addr -> ValidPtr ptr_ty (VPtr addr p).
+
+Lemma valid_ptr (ptr_ty: PtrTy) (v: Value) (H: is_valid_for (TPtr ptr_ty) v) :
+  ValidPtr ptr_ty v.
+Admitted.
+
 Lemma ptr_mono1 (ptr_ty: PtrTy) : mono1 (TPtr ptr_ty).
 Proof.
 Admitted.
@@ -14,6 +25,7 @@ Admitted.
 
 Lemma ptr_rt1 (ptr_ty: PtrTy) : rt1 (TPtr ptr_ty).
 Proof.
+intros _ v Hval.
 Admitted.
 
 Lemma int_rt2 (ptr_ty: PtrTy) : rt2 (TPtr ptr_ty).
