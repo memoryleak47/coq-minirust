0637efc529d1c647429d3981fdf57f64681827a9
memoryleak47 <memoryleak47@protonmail.ch>
2023-01-06 21:02:37 +0100

    resolve Axioms in test.v

--- test.v
+++ test.v
@@ -2,20 +2,39 @@ From Minirust.def Require Import defs encoding int_encoding utils.
 Require Import Coq.Init.Byte List NArith ZArith.
 Import ListNotations.
 
-(* TODO can we guarantee cleanly that these axioms do not escape the test suite? *)
-Axiom ptr_size8 : PTR_SIZE = 8.
-Axiom endianness_big : ENDIANNESS = BigEndian.
+Record Assumptions := {
+ A_PTR_SIZE : PTR_SIZE = 8;
+ A_ENDIANNESS : ENDIANNESS = BigEndian;
+ A_P : P = nat;
+}.
 
-Ltac exec := repeat (
+Lemma prov (A: Assumptions) (n: nat) : P.
+rewrite (A_P A).
+apply n.
+Qed.
+
+Ltac exec A := repeat (
   simpl ||
   auto ||
   split ||
   unfold encode,encode_int,encode_int_raw ||
-  rewrite ptr_size8 ||
-  rewrite endianness_big
+  rewrite (A_PTR_SIZE A) ||
+  rewrite (A_ENDIANNESS A) ||
+  rewrite (A_P A)
 ).
 
-Lemma test_encode_int :
+Lemma test_encode_int (A : Assumptions) :
 (encode (TInt 1 Unsigned) (VInt 255%Z)) = Some [Init "255" None] /\
 (encode (TInt 2 Signed) (VInt (-2)%Z)) = Some [Init "255" None; Init "254" None].
-Proof. exec. Qed.
+Proof. exec A. Qed.
+
+Lemma test_bool (A: Assumptions) :
+(encode TBool (VBool true) = Some [Init "001" None]) /\
+(encode TBool (VBool false) = Some [Init "000" None]) /\
+
+(decode TBool [Init "001" None]) = Some (VBool true) /\
+(decode TBool [Init "000" None]) = Some (VBool false) /\
+
+(decode TBool [Init "001" (Some (prov A 12))]) = Some (VBool true).
+Proof. exec A. Qed.
+
