c111ade83f0669ba588e416f8f3146b187a957a2
memoryleak47 <memoryleak47@protonmail.ch>
2023-01-08 16:52:42 +0100

    impl concat_len

--- proof/lemma/chunks.v
+++ proof/lemma/chunks.v
@@ -1,6 +1,7 @@
-Require Import List.
+Require Import List Lia.
 Import ListNotations.
 From Minirust.def Require Import utils.
+From Minirust.proof.lemma Require Import utils.
 
 Lemma chunks_len {T n k} {l: list T} (H: length l = n * k) :
   length (chunks l n) = k /\ Forall (fun x => length x = n) (chunks l n).
@@ -12,7 +13,37 @@ Lemma concat_len {T n k} {ll: list (list T)}
   (Hl2: Forall (fun x => length x = n) ll) :
   length (concat ll) = n * k.
 Proof.
-Admitted.
+generalize dependent ll.
+generalize dependent n.
+induction k as [|k]. {
+  intros.
+  rewrite ((proj1 (length_zero_iff_nil ll)) Hl1).
+  simpl.
+  lia.
+}
+
+intros n ll Hlen Hlen_inner.
+
+destruct ll.
+{ inversion Hlen. }
+
+assert (length ll = k).
+{  inversion Hlen. auto. }
+
+assert (length l = n).
+{ inversion Hlen_inner. auto. }
+
+assert (length (concat ll) = n * k). {
+  apply IHk. { assumption. }
+
+  inversion Hlen_inner.
+  auto.
+}
+
+simpl.
+rewrite app_length.
+lia.
+Qed.
 
 Lemma chunks_concat {T} {s} {l : list (list T)} (H: Forall (fun x => length x = s) l) :
   chunks (concat l) s = l.
